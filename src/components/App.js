da94ff3b7ecf1ff4f18e901440a03f08d17ad17a
guilhermeicarofr <guilhermeicarofr@gmail.com>
2022-07-30 17:41:51 -0300

    Deleting unused files, fonts import and files organization

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import TopBar from './TopBar'
+
+export default function App() {
+  return (
+    <>
+     
+    </>
+  );
+}
